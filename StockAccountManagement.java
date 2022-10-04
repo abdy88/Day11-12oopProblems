f2b97d7a9be1c5b4204d6a93eea7ad18af1cb2d7
aytech11 <aytech0110@gmail.com>
2022-10-04 19:56:12 +0530

    OOP practice problems

---
+++ StockAccountManagement.java
@@ -0,0 +1,13 @@
+package bl.com.day11;
+
+public class StockAccountManagement {
+
+	public static void main(String[] args) {
+		
+		System.out.println("******Welcome to the Stock Account Management Program******");
+		
+		StockPortfolio portfolio = new StockPortfolio();
+		portfolio.selectChoice();
+		
+	}
+}
